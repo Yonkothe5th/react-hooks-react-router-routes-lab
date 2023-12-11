4d952744ddad91086148007e0b722c4524385f0b
Brian Lawi Ochiel <lawioochiel@gmail.com>
2023-12-11 15:12:13 +0300

    completed assignment

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,25 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const directorList = directors.map((director, index) => {
+    return (
+      <div key={index}>
+        <h1>{director.name}</h1>
+        <ul>
+          {director.movies.map((movie, index) => (
+            <li key={index}>{movie}</li>
+          ))}
+        </ul>
+      </div>
+    )
+  })
+
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {directorList}
+    </div>
+  )
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,26 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const movieList = movies.map((movie, index) => {
+    return (
+      <div key={index}>
+        <h1>{movie.title}</h1>
+        <p>{movie.time}</p>
+        <ul>
+          {movie.genres.map((genre, index) => (
+            <li key={index}>{genre}</li>
+          ))}
+        </ul>
+      </div>
+    )
+  })
+
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movieList}
+    </div>
+  );
 }
 
 export default Movies;
